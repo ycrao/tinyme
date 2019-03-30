5bc9b77db1dc75c502589e1979a1581cb5751b66
raoyc <raoyc2009@gmail.com>
2019-03-30 17:36:25 +0800

    update copyright

--- app/views/footer.php
+++ app/views/footer.php
@@ -2,7 +2,7 @@
     <footer>
         <div class="container text-center">
             <p>Template from <a href="https://startbootstrap.com/template-overviews/grayscale/">Start Bootstrap</a>.</p>
-            <p>Copyright &copy; <a href="http://yas.so">YʌS Network</a> 2016</p>
+            <p>Copyright &copy; <a href="https://about.me/raoyc">raoyc</a> 2016 - <?echo date('Y')?></p>
         </div>
     </footer>
 
